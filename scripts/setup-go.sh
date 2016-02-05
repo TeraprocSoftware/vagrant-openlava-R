dcf6b3cca0f939f858f0421d0afde78ac1904c31
teraproc <teraproc.john@gmail.com>
2016-02-05 11:51:41 -0500

    update setup-go.sh

--- scripts/setup-go.sh
+++ scripts/setup-go.sh
@@ -11,7 +11,7 @@ function setupGo {
     echo export GOROOT=/usr/local/go >> /etc/profile.d/go.sh
     echo export GOPATH=/usr/local/gowork >> /etc/profile.d/go.sh
 	echo export PATH=\${GOROOT}/bin:\${GOPATH}/bin:\${PATH} >> /etc/profile.d/go.sh
-    echo export LD_LIBRARY_PATH=/usr/local/lib
+    echo export LD_LIBRARY_PATH=/usr/local/lib:\${LD_LIBRARY_PATH} >> /etc/profile.d/go.sh
     
     . /etc/profile.d/go.sh
     
