5c0ad7eca4384cb6a416f771f5480937399339e9
teraproc <teraproc.john@gmail.com>
2016-04-03 22:25:14 -0400

    after mesos and marathon

--- scripts/mesos.sh
+++ scripts/mesos.sh
@@ -1,2 +1,3 @@
 export MESOS_HOME=/usr/local/mesos
-export PATH=${MESOS_HOME}/bin:${MESOS_HOME}/sbin:${PATH}
+export PATH=${MESOS_HOME}/bin:${MESOS_HOME}/sbin:${MESOS_HOME}/libexec:${PATH}
+export MESOS_NATIVE_JAVA_LIBRARY=${MESOS_HOME}/lib
--- scripts/setup-mesos.sh
+++ scripts/setup-mesos.sh
@@ -11,8 +11,8 @@ function setupMesos {
     apt-get install -y autoconf libtool
     apt-get -y install build-essential python-dev python-boto libcurl4-nss-dev libsasl2-dev libsasl2-modules maven libapr1-dev libsvn-dev
     
+    /* http://mesos.apache.org/documentation/latest/configuration/ (/
     cd /usr/local
-    
     git clone https://github.com/apache/mesos.git mesos.git
     cd mesos.git
     git checkout -b 0.28.0 tags/0.28.0
@@ -31,12 +31,15 @@ function setupMesos {
     apt-get install sbt
     
     /* https://github.com/mesosphere/marathon/tree/releases/1.0 */
+    /* https://mesosphere.github.io/marathon/docs/command-line-flags.html */
     cd /usr/local
     git clone https://github.com/mesosphere/marathon.git marathon.git
     cd marathon
     sbt assembly
     ./bin/build-distribution
     
+    /* docker run -d -e MYID=1 -e SERVERS=olnode1 --name=zookeeper --net=host --restart=always mesoscloud/zookeeper:3.4.6-ubuntu-14.04 */
+    /* ./bin/start --master olnode1:5050 */
 }
 
 echo "setup Mesos"
